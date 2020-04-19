91062c811977ddda2ed9e0e7ed8965cf0a6ccbff
cnmy-ro <chinmay.srao97@gmail.com>
2020-04-19 13:39:50 +0200

    Added: Main files

---
+++ main.py
@@ -0,0 +1,119 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+import cv2
+import time
+
+# patch tf1 into `utils.ops`
+utils_ops.tf = tf.compat.v1
+
+# Patch the location of gfile
+tf.gfile = tf.io.gfile
+
+## Model prep
+
+def load_model(model_name):
+  model_dir = 'object_detection/models/'+ model_name + '/saved_model'
+  model = tf.saved_model.load(str(model_dir))
+  model = model.signatures['serving_default']
+  return model
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = 'object_detection/data/mscoco_label_map.pbtxt'
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+
+# DETECTION
+
+#model_name = 'ssd_mobilenet_v2_coco_2018_03_29' # ~8-9 FPS
+model_name = 'ssdlite_mobilenet_v2_coco_2018_05_09' # ~12 FPS
+detection_model = load_model(model_name)
+
+print(detection_model.inputs)
+print(detection_model.output_dtypes)
+
+
+def run_inference_for_single_image(model, image):
+  image = np.asarray(image)
+  # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+  input_tensor = tf.convert_to_tensor(image)
+  # The model expects a batch of images, so add an axis with `tf.newaxis`.
+  input_tensor = input_tensor[tf.newaxis,...]
+
+  # Run inference
+  output_dict = model(input_tensor)
+
+  # All outputs are batches tensors.
+  # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+  # We're only interested in the first num_detections.
+  num_detections = int(output_dict.pop('num_detections'))
+  output_dict = {key:value[0, :num_detections].numpy()
+                 for key,value in output_dict.items()}
+  output_dict['num_detections'] = num_detections
+
+  # detection_classes should be ints.
+  output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
+
+  # Handle models with masks:
+  if 'detection_masks' in output_dict:
+    # Reframe the the bbox mask to the image size.
+    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+              output_dict['detection_masks'], output_dict['detection_boxes'],
+               image.shape[0], image.shape[1])
+    detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
+                                       tf.uint8)
+    output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
+
+  return output_dict
+
+
+def show_inference(model, image_np):
+  # the array based representation of the image will be used later in order to prepare the
+  # result image with boxes and labels on it.
+  #image_np = np.array(Image.open(image_path))
+  # Actual detection.
+  output_dict = run_inference_for_single_image(model, image_np)
+  # Visualization of the results of a detection.
+  output = vis_util.visualize_boxes_and_labels_on_image_array(
+      image_np,
+      output_dict['detection_boxes'],
+      output_dict['detection_classes'],
+      output_dict['detection_scores'],
+      category_index,
+      instance_masks=output_dict.get('detection_masks_reframed', None),
+      use_normalized_coordinates=True,
+      line_thickness=8)
+  return output
+
+
+#for image_path in TEST_IMAGE_PATHS:
+  #show_inference(detection_model, image_path)
+
+cam = cv2.VideoCapture(0)
+while True:
+  t1 = time.time()
+  ret, frame = cam.read()
+  output = show_inference(detection_model, frame)
+  fps = 1/(time.time()-t1)
+
+  cv2.putText(output, 'FPS: {:.1f}'.format(fps), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
+  cv2.imshow('output', output)
+
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
+cv2. destroyAllWindows()
