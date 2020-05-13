dbafe89e7b82f06901470363f167b112a276988f
cnmy-ro <chinmay.srao97@gmail.com>
2020-05-13 20:33:01 +0200

    Added: Complete benchmark for all train sequences

--- run_mot16_benchmark.py
+++ run_mot16_benchmark.py
@@ -20,64 +20,62 @@ eval_dir_ssd = "./Results/Task-1/EVAL_ssd/Tracking output/"
 output_file_path = "./Results/Task-1/benchmark_results.txt"
 
 # Initialize the Py-Motmetrics accumulators
-gt_accumulator = mm.MOTAccumulator(auto_id=False)
-default_accumulator = mm.MOTAccumulator(auto_id=False)
-ssd_accumulator = mm.MOTAccumulator(auto_id=False)
+gt_accumulator = mm.MOTAccumulator(auto_id=True)
+default_accumulator = mm.MOTAccumulator(auto_id=True)
+ssd_accumulator = mm.MOTAccumulator(auto_id=True)
 
 
 ###############################################################################
-# Trials on just one sequence
 
-sequence_name = 'MOT16-02'
+# Run on all train sequences
+for sequence_name in train_sequence_names:
 
-ground_truth_path = mot_train_dir + sequence_name + '/gt/gt.txt'
-ground_truth = np.loadtxt(ground_truth_path, delimiter=',')
-ground_truth = ground_truth[ground_truth[:, 6]==1]
-ground_truth = ground_truth[:, :6]
-ground_truth = np.array(sorted(ground_truth, key=lambda gt: gt[0]))
+    ground_truth_path = mot_train_dir + sequence_name + '/gt/gt.txt'
+    ground_truth = np.loadtxt(ground_truth_path, delimiter=',')
+    # Filter out the GT entries with flag 0
+    ground_truth = ground_truth[ground_truth[:, 6]==1]
+    ground_truth = ground_truth[:, :6]
+    ground_truth = np.array(sorted(ground_truth, key=lambda gt: gt[0]))
 
-eval_result_default_path = eval_dir_default + sequence_name + '.txt'
-eval_result_default = np.loadtxt(eval_result_default_path, delimiter=',')
+    eval_result_default_path = eval_dir_default + sequence_name + '.txt'
+    eval_result_default = np.loadtxt(eval_result_default_path, delimiter=',')
 
-eval_result_ssd_path = eval_dir_ssd + sequence_name + '.txt'
-eval_result_ssd = np.loadtxt(eval_result_ssd_path, delimiter=',')
-
-
-min_frame_idx = int(min(ground_truth[:,0]))
-max_frame_idx = int(max(ground_truth[:,0]))
-
-for frame_idx in range(min_frame_idx, max_frame_idx+1):
-    gt_frame = ground_truth[ground_truth[:,0]==frame_idx]
-    gt_frame_object_ids = gt_frame[:,1]
-    gt_frame_bboxes = gt_frame[:, 2:6]
-
-    default_frame = eval_result_default[eval_result_default[:,0]==frame_idx]
-    default_frame_object_ids = default_frame[:,1]
-    default_frame_bboxes = default_frame[:,2:6]
-
-    ssd_frame = eval_result_ssd[eval_result_ssd[:,0]==frame_idx]
-    ssd_frame_object_ids = ssd_frame[:,1]
-    ssd_frame_bboxes = ssd_frame[:,2:6]
+    eval_result_ssd_path = eval_dir_ssd + sequence_name + '.txt'
+    eval_result_ssd = np.loadtxt(eval_result_ssd_path, delimiter=',')
 
 
-    # Calculate distance  matrix (IoU)...
-    self_dist_matrix = mm.distances.iou_matrix(gt_frame_bboxes, gt_frame_bboxes)
-    gt_accumulator.update(gt_frame_object_ids,   # All ground truth objects (IDs)
-                          gt_frame_object_ids, # Predicted objects (IDs)
-                          self_dist_matrix,
-                          frameid=frame_idx)
+    min_frame_idx = int(min(ground_truth[:,0]))
+    max_frame_idx = int(max(ground_truth[:,0]))
 
-    default_distance_matrix = mm.distances.iou_matrix(gt_frame_bboxes, default_frame_bboxes)
-    default_accumulator.update(gt_frame_object_ids,   # All ground truth objects (IDs)
-                           default_frame_object_ids, # Predicted objects (IDs)
-                           default_distance_matrix,
-                           frameid=frame_idx)
+    for frame_idx in range(min_frame_idx, max_frame_idx+1):
+        gt_frame = ground_truth[ground_truth[:,0]==frame_idx]
+        gt_frame_object_ids = gt_frame[:,1]
+        gt_frame_bboxes = gt_frame[:, 2:6]
 
-    ssd_distance_matrix = mm.distances.iou_matrix(gt_frame_bboxes, ssd_frame_bboxes)
-    ssd_accumulator.update(gt_frame_object_ids,   # All ground truth objects (IDs)
-                           ssd_frame_object_ids, # Predicted objects (IDs)
-                           ssd_distance_matrix,
-                           frameid=frame_idx)
+        default_frame = eval_result_default[eval_result_default[:,0]==frame_idx]
+        default_frame_object_ids = default_frame[:,1]
+        default_frame_bboxes = default_frame[:,2:6]
+
+        ssd_frame = eval_result_ssd[eval_result_ssd[:,0]==frame_idx]
+        ssd_frame_object_ids = ssd_frame[:,1]
+        ssd_frame_bboxes = ssd_frame[:,2:6]
+
+
+        # Calculate distance  matrix (IoU)...
+        self_dist_matrix = mm.distances.iou_matrix(gt_frame_bboxes, gt_frame_bboxes)
+        gt_accumulator.update(gt_frame_object_ids,   # All ground truth objects (IDs)
+                              gt_frame_object_ids, # Predicted objects (IDs)
+                              self_dist_matrix)
+
+        default_distance_matrix = mm.distances.iou_matrix(gt_frame_bboxes, default_frame_bboxes)
+        default_accumulator.update(gt_frame_object_ids,   # All ground truth objects (IDs)
+                               default_frame_object_ids, # Predicted objects (IDs)
+                               default_distance_matrix)
+
+        ssd_distance_matrix = mm.distances.iou_matrix(gt_frame_bboxes, ssd_frame_bboxes)
+        ssd_accumulator.update(gt_frame_object_ids,   # All ground truth objects (IDs)
+                               ssd_frame_object_ids, # Predicted objects (IDs)
+                               ssd_distance_matrix)
 
 
 
