ff8d0104c91865018bb4f9db959afcc2630ce2ee
cnmy-ro <chinmay.srao97@gmail.com>
2020-05-25 03:43:37 +0200

    Update runExpt_vehicles_cosineThresh.sh

--- runExpt_vehicles_cosineThresh.sh
+++ runExpt_vehicles_cosineThresh.sh
@@ -14,12 +14,12 @@ echo Bechmarking all 3 detectors for cosine threshold $max_cosine_distance
 
 for detector in "DPM" "RCNN" "SSD"
 do
-	python python track_vehicles.py --detector $detector \
+	python run_vehicle_tracker.py --detector $detector \
 	                                --online_detection $online_detection \
 	                                --min_confidence $min_confidence \
 	                                --max_cosine_distance $max_cosine_distance \
 	                                --display $display
 
-	python vehicle_benchmark.py --output_path $benchmark_results_path
+	python run_detrac_benchmark.py --output_path $benchmark_results_path
 done
 
